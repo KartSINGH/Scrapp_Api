4fc1e14bf39f3606269cfab41b7744d4e8a3f7fd
kartik singh <kartiksingh@kartiks-MacBook-Air.local>
2017-03-17 11:49:25 +0530

    host 0

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 var sequelize = require('sequelize'),
 seq=new sequelize('scrapp','35.154.195.217','scrappfree',{
-    host:'35.154.195.217',
+    host:'0.0.0.0',
  dialect:'mysql',
     pool:{
         max:15,
