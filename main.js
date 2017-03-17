9ed034a0093235d4b4db6e50af04efe58f2d332b
kartik singh <kartiksingh@kartiks-MacBook-Air.local>
2017-03-17 11:13:11 +0530

    changes2

--- main.js
+++ main.js
@@ -8,12 +8,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
-
-<<<<<<< HEAD
-=======
 app.use(bodyparser.json())
-
->>>>>>> e7f42a7eff4c8e88b5207d9b3a27471d71b9c50c
 app.use(bodyparser.urlencoded({
     extended: true
 }))
